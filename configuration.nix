d6b55ea67c5ea68cd3323319aa6ebe209d3b67fe
Jeanie Lam <jeanielamwh@gmail.com>
2023-07-18 01:13:19 +0000

    Use i3

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,21 @@
     };
   };
 
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+      lightdm.enable = true;
+    };
+
+    windowManager.i3.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     git
     gnumake
